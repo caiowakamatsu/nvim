d039c6bc0cc1823a715bdbeb615059d94e2b6a53
caiowakamatsu <caiodcw@gmail.com>
2025-08-05 02:45:56 -0300

    close tree when file opened

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -16,11 +16,26 @@ return {
 		build = ":TSUpdate",
 	},
 
+
 	{
 		"nvim-tree/nvim-tree.lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("nvim-tree").setup()
+			require("nvim-tree").setup({
+					on_attach = function(bufnr)
+						local api = require("nvim-tree.api")
+
+						vim.keymap.set('n', '<CR>', function()
+						local node = api.tree.get_node_under_cursor()
+						api.node.open.edit()
+
+						if not node or node.nodes == nil then
+							api.tree.close()
+						end
+					end, { buffer = bufnr, noremap = true, silent = true })
+				end,
+			})
+
 			vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 		end,
 	},
